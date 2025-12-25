016a4e3b33e2be5d78bbfa0683b25ac9a6825e22
Harshvardhan Dhakad <harshvardhansinghdhakad@gmail.com>
2025-12-25 12:20:08 +0530

    Fix AuthContext import path

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { AuthProvider } from '../../contexts/AuthContext';
+import { AuthProvider } from '../contexts/AuthContext';
 
 export const unstable_settings = {
   anchor: '(tabs)',
